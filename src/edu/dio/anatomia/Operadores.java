dd86903633770aa21cd7320ce1705f9dcf117965
Dragnipurake <aalmirsantoss@gmail.com>
2024-06-19 21:02:08 -0300

    Segundo Commit - operadores

---
+++ src/edu/dio/anatomia/Operadores.java
@@ -0,0 +1,10 @@
+package edu.dio.anatomia;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+		System.out.println(nomeCompleto);
+	}
+}
